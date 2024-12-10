6656e048e464a42ad29917d8ceb36d9e886ae452
sebbyfay1 <sebby.fay@gmail.com>
2024-12-10 10:44:59 -0600

    Initial Commit

---
+++ DogThoughtsApp/DogThoughtsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogThoughtsAppApp.swift
+//  DogThoughtsApp
+//
+//  Created by Sebastian Fay on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DogThoughtsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
